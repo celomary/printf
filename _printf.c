1a9e7e8ae8ceb3282abd56e7f73d16aeda3996a5
Mohamed Elomary <elomarymohamed72@gmail.com>
2023-03-29 00:01:50 +0000

    working on second test case printf

--- _printf.c
+++ _printf.c
@@ -5,12 +5,21 @@
  *
  * @format: format to check for white space
  * @iterator: pointer to current position in the format
- * Return: nothing
+ * Return: printed characters
  */
-void	skip_whitespace(const char *format, int *iterator)
+int		skip_whitespace(const char *format, int *iterator)
 {
+	int		to_check;
+
+	to_check = format[*iterator + 1] == ' ';
 	while (format[++(*iterator)] == ' ')
 	;
+	if (to_check && (format[*iterator] == 'd' || format[*iterator] == 'i'))
+	{
+		_putchar(' ');
+		return (1);
+	}
+	return (0);
 }
 /**
  * _printf - function that produce output according to the format
@@ -33,7 +42,7 @@ int	_printf(const char *format, ...)
 	{
 		if (format[iterator] == '%')
 		{
-			skip_whitespace(format, &iterator);
+			printed = skip_whitespace(format, &iterator);
 			if (_is_inset(format[iterator], AVAILABLE_CONVERSION))
 				printed += _conversion_handler(&ap, format[iterator]);
 			else
